29e877e8c33f1489c67683582ac3bc0f77f68fb3
YeowonNa <mhyh716@daum.net>
2022-04-28 18:04:12 +0900

    add createReview

--- movie/src/components/ReviewForm.js
+++ movie/src/components/ReviewForm.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
+import { createReview } from "../api";
 import FileInput from "./FileInput";
 import RatingInput from "./RatingInput";
 
+const INITIAL_VALUES = {
+  title: "",
+  rating: 0,
+  content: "",
+  imgFile: null,
+};
+
 function ReviewForm(props) {
-  const [values, setValues] = useState({
-    title: "",
-    rating: 0,
-    content: "",
-    imgFile: null,
-  });
+  const [values, setValues] = useState(INITIAL_VALUES);
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -22,10 +25,17 @@ function ReviewForm(props) {
     handleChange(name, value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(values);
+    const formData = new FormData();
+    formData.append("title", values.title);
+    formData.append("rating", values.rating);
+    formData.append("content", values.content);
+    formData.append("imgFile", values.imgFile);
+    createReview(formData);
+    setValues(INITIAL_VALUES);
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <FileInput
