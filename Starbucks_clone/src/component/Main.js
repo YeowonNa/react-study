2729b0ae05a43beee8a1e11b2ca2267f36c4ce67
YeowonNa <mhyh716@daum.net>
2022-05-04 14:57:18 +0900

    이미지 위치 변경

--- Starbucks_clone/src/component/Main.js
+++ Starbucks_clone/src/component/Main.js
@@ -6,14 +6,21 @@ function Main() {
       <div className="container">
         <div className="toWrap">
           <div className="main_wrap">
-            <div className="slogan">
-              <img src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Summer1_main_slogun.png"></img>
-            </div>
-            <div>
-              <img src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Summer1_main_punchgraffiti3.png"></img>
-            </div>
-            <div>
-              <img src="https://image.istarbucks.co.kr/upload/common/img/main/2022/m_2022_Summer1_main_rollinmint.png"></img>
+            <div className="main_wrap_inner">
+              <div className="slogan">
+                <img src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Summer1_main_slogun.png"></img>
+              </div>
+              <div className="main_set">
+                <div className="set_common set_01">
+                  <img src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Summer1_main_rollinmint.png"></img>
+                </div>
+                <div className="set_common set_02">
+                  <img src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Summer1_main_punchgraffiti3.png"></img>
+                </div>
+                <div className="set_common set_03">
+                  <img src="https://image.istarbucks.co.kr/upload/common/img/main/2022/2022_Summer1_main_pomeloflow.png"></img>
+                </div>
+              </div>
             </div>
           </div>
         </div>
